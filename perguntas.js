012cd1d13b6a301f61ce260a99c718a1fdd81387
João <00001132543162SP@al.educacao.sp.gov.br>
2024-12-04 18:12:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
+
+criaCartao (
+    'Geogafia',
+    'Qual a capital do Canadá?',
+    'Ottawa'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da China?',
+    'Pequim'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital do Japão?',
+    'Tóquio'
+)
